f80c525206e6d0569f4adad4f5a0bd538d31e27b
Nikita Derevskov <nikitaderevskov@gmail.com>
2020-12-06 00:08:33 +0300

    main.js: add context menu and copy by cmd c in mac

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu} = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -11,9 +11,44 @@ function createWindow () {
 
   win.loadFile('index.html')
   win.webContents.openDevTools()
+
+  return win;
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  const win = createWindow()
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'Menu',
+      submenu: [
+        {role: 'reload'},
+        {role: 'undo'},
+        {role: 'redo'},
+        {role: 'quit'},
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+        { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+        { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+      ]
+    }
+  ])
+  Menu.setApplicationMenu(menu)
+
+  win.webContents.on('context-menu', (e, props) => {
+    const {selectionText} = props;
+    if (selectionText) {
+       const mainMenu = Menu.getApplicationMenu().items.filter(function(item){
+      return item.label == "Edit";
+    })[0].submenu;
+       mainMenu.popup(win);
+    }
+  })
+
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -23,6 +58,6 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    // createWindow()
   }
 })
